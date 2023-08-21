5a35db2043b032b81d6e2524a994ed3afc19fc03
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2023-08-21 23:53:14 +0700

    Commits

--- AfahIyh/MainContent.swift
+++ AfahIyh/MainContent.swift
@@ -9,38 +9,38 @@ import SwiftUI
 
 struct MainContent: View {
     
-//    @State var path : [Int] = []
+    //    @State var path : [Int] = []
     @State var toNavigate : Bool = false
     
     var body: some View {
-//        NavigationStack(path: $path){
-            ZStack{
-                Image("LandingPagewTitle")
-                    .resizable()
-                    .frame(width: 850.0, height: 500)
-                    .offset(x: 0, y: 30)
-                    .ignoresSafeArea()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.white, lineWidth: 15)
-                            .frame(width: 150,height: 50)
-                            .overlay(RoundedRectangle(cornerRadius: 20)
-                                .fill(Color.green)
-                                .overlay(
-                                    Text("Mulai")
-                                        .bold()
-                                        .font(.system(size: 34 )).multilineTextAlignment(.center).padding(15)
-                                )
-                            ).offset(x: 0, y: 120)
-                            .onTapGesture {
-//                                path.append(1)
-                                self.toNavigate.toggle()
-                            }
-                            .navigationDestination(isPresented: $toNavigate) {
-                                IntroContent().navigationBarBackButtonHidden()
-                            }
-                    )
-            }
+        //        NavigationStack(path: $path){
+        ZStack{
+            Image("LandingPagewTitle")
+                .resizable()
+                .frame(width: 850.0, height: 500)
+                .offset(x: 0, y: 30)
+                .ignoresSafeArea()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white, lineWidth: 15)
+                        .frame(width: 150,height: 50)
+                        .overlay(RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.green)
+                            .overlay(
+                                Text("Mulai")
+                                    .bold()
+                                    .font(.system(size: 34 )).multilineTextAlignment(.center).padding(15)
+                            )
+                        ).offset(x: 0, y: 120)
+                        .onTapGesture {
+                            //                                path.append(1)
+                            self.toNavigate.toggle()
+                        }
+                        .navigationDestination(isPresented: $toNavigate) {
+                            IntroContent().navigationBarBackButtonHidden()
+                        }
+                )
+        }
     }
 }
 
